200db0e16cd87aa0cbd4f362715b4072f2906ffd
Rolla Manikanta <rollamanikanta@gmail.com>
2025-03-21 23:51:35 +0530

    d

--- Basic-Counter-app-master/app/src/main/java/com/example/counterapp/MainActivity.kt
+++ Basic-Counter-app-master/app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
     private var counter=0
     override fun onCreate(savedInstanceState: Bundle?) {
